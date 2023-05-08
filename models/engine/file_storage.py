6274f77814b0a45d079fa56c5735e63698aa56e4
samuelselasi <samelsekasi@gmail.com>
2023-05-08 19:56:37 +0000

    Fix text formatting in file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,36 +1,42 @@
 #!/usr/bin/python3
-""" File storage module for AirBnB clone."""
+"""File storage module for AirBnB clone."""
 
 import json
 import os
 
 
 class FileStorage:
-	"""Class to store and retrieve data from Console."""
-		__file_path = "file.json"
-		__objects = {}
+    """Class to store and retrieve data from Console."""
 
-		def all(self):
-			"""Function that returns all stored __objects."""
-				return (__objects)
+    __file_path = "file.json"
+    __objects = {}
 
-		def new(self, obj):
-			"""Function that sets __objects with class-name key."""
-				class_key = "{}.{}".format(type(obj).__name, obj.id)
-				self.__objects[class_key] = obj
+    def all(self):
+        """Function that returns all stored __objects."""
 
-		def save(self):
-			"""Function that saves __objects to JSON file."""
-				with open(self.__file_path, "w", encoding="utf-8") as f:
-				obj = {k: v.to_dict() for k, v in self.__object.items()}
-					json.dump(obj, f)
+        return (__objects)
 
-		def reload(self):
-			"""Function that deserializes JSON file to __objects."""
+    def new(self, obj):
+        """Function that sets __objects with class-name key."""
 
-				if not os.path.isfile(self.__file_path):
-					return
+        class_key = "{}.{}".format(type(obj).__name, obj.id)
+        self.__objects[class_key] = obj
 
-				with open(self.__file_path, "r", encoding="utf-8") as f:
-				obj_dict = json.load(f)
-				obj_dict = {k: self.classes()[v["__class__"]](**v) for k, v in obj_dict.items()}
+    def save(self):
+        """Function that saves __objects to JSON file."""
+
+        with open(self.__file_path, "w", encoding="utf-8") as f:
+            obj = {k: v.to_dict() for k, v in self.__object.items()}
+            json.dump(obj, f)
+
+    def reload(self):
+        """Function that deserializes JSON file to __objects."""
+
+        if not os.path.isfile(self.__file_path):
+            return
+
+        with open(self.__file_path, "r", encoding="utf-8") as f:
+            obj_dict = json.load(f)
+            obj_dict = {k: self.classes()[v["__class__"]](**v) for
+                        k, v in obj_dict.items()}
+            self.__objects = obj_dict
