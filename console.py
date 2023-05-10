691c093bd35f5a60858aa43d711b1baf1cb2d8fb
SKGakiria <swgakiria@gmail.com>
2023-05-10 11:28:48 -0700

    Update console.py with create and show commands

--- console.py
+++ console.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """The Command Line Interpreter."""
 
+from models import *
 import cmd
 
 
@@ -24,6 +25,41 @@ class HBNBCommand(cmd.Cmd):
         """Function that does nothing on ENTER."""
 
         pass
+
+    def do_create(self, line):
+        """Function that creates new instance of BaseModel."""
+
+        if line == "" or line is None:
+            print("** class name missing **")
+
+        elif line not in storage.classes():
+            print("** class doesn't exist **")
+
+        else:
+            instance = storage.classes()[line]()
+            instance.save()
+            print(instance)
+
+    def do_show(self, line):
+        """Function that prints the string representation of an instance."""
+
+        if line == "" or line is None:
+            print("** class name missing **")
+
+        else:
+            words = line.split(' ')
+            if words[0] not in storage.classes():
+                print("** class doesn't exist **")
+
+            elif len(words) < 2:
+                print("** instance id missing **")
+
+            else:
+                patt = "{}.{}".format(words[0], words[1])
+                if patt not in storage.all():
+                    print("** no instance found **")
+                else:
+                    print(storage.all()[patt])
 
 
 if __name__ == '__main__':
