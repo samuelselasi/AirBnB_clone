bb354b2f4c422c2d771d29630416fbfcf6b4c765
samuelselasi <samelsekasi@gmail.com>
2023-05-11 00:31:00 +0000

    Test console.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """File storage module for AirBnB clone."""
 
+from datetime import datetime
 import json
 import os
 
@@ -60,3 +61,42 @@ class FileStorage:
             obj_dict = {k: self.classes()[v["__class__"]](**v) for
                     k, v in obj_dict.items()}
             self.__objects = obj_dict
+
+    def attributes(self):
+        """Function that returns class instances and their attributes"""
+
+        attributes = {
+            "BaseModel":
+                     {"id": str,
+                      "created_at": datetime,
+                      "updated_at": datetime},
+            "User":
+                     {"email": str,
+                      "password": str,
+                      "first_name": str,
+                      "last_name": str},
+            "State":
+                     {"name": str},
+            "City":
+                     {"state_id": str,
+                      "name": str},
+            "Amenity":
+                     {"name": str},
+            "Place":
+                     {"city_id": str,
+                      "user_id": str,
+                      "name": str,
+                      "description": str,
+                      "number_rooms": int,
+                      "number_bathrooms": int,
+                      "max_guest": int,
+                      "price_by_night": int,
+                      "latitude": float,
+                      "longitude": float,
+                      "amenity_ids": list},
+            "Review":
+            {"place_id": str,
+                         "user_id": str,
+                         "text": str}
+        }
+        return (attributes)
